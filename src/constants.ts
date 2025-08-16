b7eb25c49cd74656b9a19c9a99cae788ac95db8a
Brandon Watkins <bwat47@gmail.com>
2025-08-16 17:09:34 -0400

    update comments

--- src/constants.ts
+++ src/constants.ts
@@ -13,7 +13,12 @@ export const REGEX_PATTERNS = {
 	CODE_BLOCKS: /(```[\s\S]*?```|`[^`\n]*`)/g,
 	HTML_IMG: /<img[^>]*>/gi,
 	MARKDOWN_IMG: /!\[[^\]]*\]\(:\/{1,2}[a-f0-9]{32}\)/gi,
+	// Matches HTML <img> tags with a Joplin resource ID in the src attribute.
+    // Group 1: All attributes up to the resource ID
+    // Group 2: The 32-character Joplin resource ID
 	HTML_IMG_WITH_RESOURCE: /<img([^>]*src=["']:\/{1,2}([a-f0-9]{32})["'][^>]*)>/gi,
+	// Matches HTML <img> tags with any resource ID (not just 32 chars).
+    // Group 1: The resource ID (any non-quote, non-angle-bracket sequence)
 	IMG_TAG_WITH_RESOURCE: /<img[^>]*src=["']:?\/{1,2}([^"'>]+)["'][^>]*>/gi,
 };
 
