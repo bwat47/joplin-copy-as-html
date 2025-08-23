81449b4e2729716766454298b15a923de6e435ca
Brandon Watkins <bwat47@gmail.com>
2025-08-22 21:45:01 -0400

    improve css font stack

--- src/defaultStylesheet.ts
+++ src/defaultStylesheet.ts
@@ -19,70 +19,86 @@
  */
 
 export const defaultStylesheet = `
-body, input {
-  font-family: Roboto, Arial, "Helvetica Neue", Helvetica, sans-serif;
+body,
+input {
+	font-family: Aptos, Calibri, Arial, 'Helvetica Neue', Helvetica, sans-serif;
 }
 
-code, kbd, pre {
-  font-family: "Roboto Mono", Monaco, "Courier New", Courier, monospace;
+code,
+kbd,
+pre {
+	font-family: 'Aptos Mono', 'Cascadia Code', 'Consolas', 'Source Code Pro', Menlo, Monaco, 'DejaVu Sans Mono', monospace;
 }
 
 pre {
-  padding: 0.5em;
+	padding: 0.5em;
 }
 
 p {
-  margin-top: 1em;
-  margin-bottom: 1em;
+	margin-top: 1em;
+	margin-bottom: 1em;
 }
 
 table {
-  border-collapse: collapse;
-  border: 1px solid #ccc;
+	border-collapse: collapse;
+	border: 1px solid #ccc;
 }
 
-th, td {
-  border: 1px solid #eee;
-  padding: 0.5em;
+th,
+td {
+	border: 1px solid #eee;
+	padding: 0.5em;
 }
 
 blockquote {
-  border-left: 3px solid #ccc;  /* left border typical for blockquotes */
-  margin: 1.5em 0 1.5em 0.4em;
-  padding-left: 0.5em;
+	border-left: 3px solid #ccc;
+	/* left border typical for blockquotes */
+	margin: 1.5em 0 1.5em 0.4em;
+	padding-left: 0.5em;
 }
 
 hr {
-  border: none;
-  border-top: 1px solid #ccc;
-  margin: 1em 0;
+	border: none;
+	border-top: 1px solid #ccc;
+	margin: 1em 0;
 }
 
-h1, h2, h3, h4, h5, h6 {
-  margin-top: 1em;
-  margin-bottom: 0.5em;
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+	margin-top: 1em;
+	margin-bottom: 0.5em;
 }
 
-ul, ol {
-  margin-top: 1em;
-  margin-bottom: 1em;
+ul,
+ol {
+	margin-top: 1em;
+	margin-bottom: 1em;
 }
 
-ul > li, ol > li {
-  margin-bottom: 0.5em;
+ul>li,
+ol>li {
+	margin-bottom: 0.5em;
 }
 
-ul ul, ol ol, ul li > ul, ol li > ol {
-  margin-top: 0.5em;
-  margin-bottom: 0.5em;
+ul ul,
+ol ol,
+ul li>ul,
+ol li>ol {
+	margin-top: 0.5em;
+	margin-bottom: 0.5em;
 }
 
 /* Joplin checkboxes */
 li.md-checkbox,
 li.task-list-item {
-    list-style: none; /* remove bullet point. */
-    padding-left: 0;
-    margin-left: 0;
+	list-style: none;
+	/* remove bullet point. */
+	padding-left: 0;
+	margin-left: 0;
 
 }
 
@@ -91,13 +107,14 @@ li.md-checkbox input[type="checkbox"],
 li.md-checkbox label,
 li.task-list-item input[type="checkbox"],
 li.task-list-item label {
-    display: inline-block;
-    vertical-align: middle; /* This is the key for alignment. */
+	display: inline-block;
+	vertical-align: middle;
+	/* This is the key for alignment. */
 }
 
 /* Add space to the right of the checkbox. */
 li.md-checkbox input[type="checkbox"],
 li.task-list-item input[type="checkbox"] {
-    margin-right: 0.5em;
+	margin-right: 0.5em;
 }
 `;
