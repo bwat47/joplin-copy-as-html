e054d17afd68eac46ac670178d3c7ed3f3a27dae
bwat47 <bwat47@gmail.com>
2026-08-20 07:47:59 -0400

    chore: update eslint config and agents.md

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -38,6 +38,8 @@ export default [
             // report an error if any circular dependency is found
             'import/no-cycle': ['error', { maxDepth: Infinity }],
             'sonarjs/dompurify-unsafe-config': 'off',
+            '@typescript-eslint/no-inferrable-types': 'error',
+            '@typescript-eslint/explicit-module-boundary-types': 'error',
         },
     },
 
